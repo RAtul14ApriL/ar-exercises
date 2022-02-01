78f207b8798955c0015238e7c488f2f36b3e47e5
Md Hefzul Bari <ratul14april@gmail.com>
2022-02-01 17:14:17 -0600

    Updated store-1 record

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+pp @store1
+pp @store2
+
+@store1.update(annual_revenue: 350000)
+
+pp @store1
